4773676dc37b32812b29a4dcb5901ba08fb1c23f
gahaku200 <59167773+gahaku200@users.noreply.github.com>
2020-01-05 21:58:46 +0900

    ajax 3

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -41,26 +41,28 @@ $(function(){
    };
  }
 $('#new_message').on('submit', function(e){
- e.preventDefault();
- var formData = new FormData(this);
- var url = $(this).attr('action')
- $.ajax({
-   url: url,
-   type: "POST",
-   data: formData,
-   dataType: 'json',
-   processData: false,
-   contentType: false
- })
-  .done(function(data){
-    var html = buildHTML(data);
-    $('.messages').append(html);
-    $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});      
-    $('form')[0].reset();
-    $('input').prop('disabled', false);
-  })
-  .fail(function() {
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action')
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.messages').append(html);
+      $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});      
+      $('form')[0].reset();
+      html.always(function(){
+      $('input').prop('disabled', false);
+      })
+    })
+    .fail(function() {
     alert("メッセージ送信に失敗しました");
-  });
-})
+    });
+  })
 });
